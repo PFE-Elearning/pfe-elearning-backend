2cbcbe018d90d427ee7d14768db8d3b261991dce
MAALOUM-SAID <saaid_120@hotmail.com>
2023-06-21 03:12:08 +0100

    create migrations

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
-
+use App\Models\Formation;
 return new class extends Migration
 {
     /**
@@ -13,10 +13,15 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('first_name');
+            $table->string('last_name');
             $table->string('email')->unique();
+            $table->string('phone')->unique();
+            $table->string('avatar')->nullable();
+            $table->string('role');
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->ForeignIdFor(Formation::class)->nullable();
             $table->rememberToken();
             $table->timestamps();
         });

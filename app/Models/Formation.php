f0107b76b61c7c0e6d792dbfa3b2f93c674962a9
MAALOUM-SAID <saaid_120@hotmail.com>
2023-06-21 22:20:24 +0100

    merge payments-api

--- app/Models/Formation.php
+++ app/Models/Formation.php
@@ -4,12 +4,8 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-<<<<<<< HEAD
 use App\Models\User; 
 use App\Models\Course;
-=======
-use App\Models\User;
->>>>>>> 2d4f597ae2f437113f388596e84256dc72eb4727
 class Formation extends Model
 {
     use HasFactory;
@@ -20,10 +16,7 @@ class Formation extends Model
     public function users(){
         return $this->hasMany(User::class);
     }
-<<<<<<< HEAD
     public function courses(){
         return $this->hasMany(Course::class);
     }
-=======
->>>>>>> 2d4f597ae2f437113f388596e84256dc72eb4727
 }
